3e2a3ddb44e3fc3cf733d665ff399092d00911bc
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-02-13 21:31:27 -0800

    Added conditional evaluation of calculation and created simple loop

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -7,28 +7,49 @@ namespace COMP003A.Assignment5
         static void Main(string[] args)
         {
             // displays beginning text
-            IntroMessage("Welcome to the Program Organizer!\n");
+            IntroMessage("Welcome to the Program Organizer!");
 
-            // user input
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            while (true)
+            {
 
-            Console.Write("Enter your age: ");
-            int num = int.Parse(Console.ReadLine());
 
-            // calculation results
-            int constant = 5;
-            int result = AgeCalculation(num, constant);
-            Console.WriteLine($"Calculated Value: {result}");
+                // user input
+                Console.Write("\n\nEnter your name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter your age: ");
+                int age = int.Parse(Console.ReadLine());
+
+
+                // calculation results
+                int constant = 5;
+                int result = AgeCalculation(age, constant);
+                Console.WriteLine($"\nCalculated Value: {result}");
+
+                // evaluation
+                if (result > 20)
+                {
+                    Console.WriteLine("Your number is greater than 20");
+                }
+                else
+                {
+                    Console.WriteLine("Your number is less than or equal to 20");
+                }
+
+                // user input for repetition
+                Console.Write("\nWould you like to restart the program? (y/n): ");
+
+            }
         }
         static void IntroMessage(string message)
         {
             // first message
-            Console.WriteLine(message);
+            Console.Write(message);
         }
-        static int AgeCalculation(int num, int constant)
+        static int AgeCalculation(int age, int constant)
         {
-            return num + constant;
+            // calculation adding 5 to age
+            return age + constant;
         }
     }
 }
