0ec12967aac3d0c90924f422df2d95bcd76ab17a
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-02-13 21:18:51 -0800

    Added calculation based on age and output of that info

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -1,23 +1,34 @@
-﻿namespace COMP003A.Assignment5
+﻿using System.Reflection.Metadata;
+
+namespace COMP003A.Assignment5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Displays text
+            // displays beginning text
             IntroMessage("Welcome to the Program Organizer!\n");
 
+            // user input
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
 
             Console.Write("Enter your age: ");
             int num = int.Parse(Console.ReadLine());
 
+            // calculation results
+            int constant = 5;
+            int result = AgeCalculation(num, constant);
+            Console.WriteLine($"Calculated Value: {result}");
         }
         static void IntroMessage(string message)
         {
             // first message
             Console.WriteLine(message);
+        }
+        static int AgeCalculation(int num, int constant)
+        {
+            return num + constant;
         }
     }
 }
